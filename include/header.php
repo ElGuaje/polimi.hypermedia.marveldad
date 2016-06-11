3f1c122dc6c87d33186810d04a98b549da852ad8
panyushov <panyushov@github>
2016-06-11 14:38:19 +0200

    Extracts css styles into extenral general css file

--- include/header.php
+++ include/header.php
@@ -15,66 +15,13 @@
 		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 		<link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
 
+		<link href="css/general.css" rel="stylesheet">
+
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
 		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
-		<style>
-			html {
-			  position: relative;
-			  min-height: 100%;
-			}
-			body {
-			  /* Margin bottom by footer height */
-			  margin-bottom: 60px;
-			  padding-top: 70px;
-
-			}
-			.footer {
-			  position: absolute;
-			  bottom: 0;
-			  width: 100%;
-			  /* Set the fixed height of the footer here */
-			  height: 60px;
-			  background-color: #f5f5f5;
-			}
-			body > .container {
-			  padding: 60px 15px 0;
-			}
-			.container .text-muted {
-			  margin: 20px 0;
-			}
-
-			.footer > .container {
-			  padding-right: 15px;
-			  padding-left: 15px;
-			}
-			code {
-			  font-size: 80%;
-			}
-            .nav {
-                height: 75px;
-            }
-            .navbar-default {
-                background-color: #004592;
-                border-color: #003E83;
-            }
-            .navbar-default .navbar-nav > li > a {
-                color: lightgray;
-            }
-            .navbar-default .navbar-nav > li > a:hover, .navbar-default .navbar-nav > li > a:focus {
-                color: #ff3c1a;
-                background-color: transparent;
-            }
-            .nav-container {
-                padding-top: 25px;
-            }
-            .navbar-brand {
-                padding-top: 0px;
-                margin-top: -8px;
-            }
-		</style>
 	</head>
 	<body>
 	<?php
