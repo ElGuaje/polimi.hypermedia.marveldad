f776adecf5049d4abf3803a0d54c9bd9514855b4
Marco <blackhammer94@gmail.com>
2016-06-13 12:39:57 +0200

    Adds style to the product.php page

--- product.php
+++ product.php
@@ -2,8 +2,8 @@
 	include 'include/header.php';
 ?>
 	<div class="container">
-		<div class="col-3">
-			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		<div class="col-md-3 col-xs-12">
+			<div id="myCarousel" class="carousel slide small-carousel" data-ride="carousel">
 					<!-- Indicators -->
 					<ol class="carousel-indicators">
 						<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
@@ -35,8 +35,31 @@
 					</div>
 				</div>
 		</div>
-        <div class="col-9">
-            <p>Hello</p>
+        <div class="col-md-9 col-xs-12">
+            <div id="nomeProdotto">
+                <h1>NOME PRODOTTO</h1>
+            </div>
+            <ul class="nav nav-tabs">
+              <li class="active"><a data-toggle="tab" href="#home">Home</a></li>
+              <li><a data-toggle="tab" href="#menu1">Menu 1</a></li>
+              <li><a data-toggle="tab" href="#menu2">Menu 2</a></li>
+              <li><a data-toggle="tab" href="#">Menu 3</a></li>
+            </ul>
+                
+            <div class="tab-content">
+              <div id="home" class="tab-pane fade in active">
+                <h3>HOME</h3>
+                <p>Some content.</p>
+              </div>
+              <div id="menu1" class="tab-pane fade">
+                <h3>Menu 1</h3>
+                <p>Some content in menu 1.</p>
+              </div>
+              <div id="menu2" class="tab-pane fade">
+                <h3>Menu 2</h3>
+                <p>Some content in menu 2.</p>
+              </div>
+            </div>
         </div>
 	</div>
 <?php	
