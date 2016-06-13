004c0005f9768b4967be5e313d7d17b0213382c1
Davide Piantella <davide.piantella@gmail.com>
2016-06-13 11:30:50 +0200

    Loading js at the top of the page (temp)

--- include/header.php
+++ include/header.php
@@ -22,6 +22,14 @@
 		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
+			<!-- Bootstrap core JavaScript
+			================================================== -->
+			<!-- Placed at the end of the document so the pages load faster -->
+			<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+			<script>window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>')</script>
+			<script src="js/bootstrap.min.js"></script>
+			<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+			<script src="js/ie10-viewport-bug-workaround.js"></script>
 	</head>
 	<body>
 	<?php
