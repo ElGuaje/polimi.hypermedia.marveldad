422d2c937d1fb7e85a4004631dd5acbc04323ee9
panyushov <panyushov@github>
2016-06-18 18:25:05 +0200

    Adds SL Offers page

---
+++ sl_promo.php
@@ -0,0 +1,72 @@
+<?php
+include 'include/header.php';
+?>
+    <div class="container">
+        <!--Header row -->
+        <div class="row">
+            <div class="col-xs-12">
+                <h1 class="text-center">Smart life - offerte per te</h1>
+            </div>
+        </div>
+        <!--Cards with promotions -->
+        <div class="row">
+            <div class="row">
+                <div class="col-sm-6 col-md-4">
+                    <div class="wrapper">
+                        <span class="promo-discount">20%</span>
+                        <div class="thumbnail sl-offerta">
+                            <p>
+                                <img class="media-object" src="http://placehold.it/200x150" align="left" alt="First">
+                            <h4 class="media-heading">Tim reading</h4>
+                            Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della
+                            stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo
+                            secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò
+                            per preparare un testo campione. È sopravvissuto non solo a più di cinque secoli, ma
+                            anche al passaggio alla videoimpaginazione, pervenendoci sostanzialmente inalterato.
+                            </p>
+                            <h4 class="text-center">20% di sconto per te per i prossimi 2 mesi</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="wrapper">
+                        <span class="promo-gift"><p class="text-center">+</p></span>
+                        <div class="thumbnail sl-offerta">
+                            <p>
+                                <img class="media-object" src="http://placehold.it/200x150" align="left" alt="First">
+                            <h4 class="media-heading">Tim trasporti</h4>
+                            Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della
+                            stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo
+                            secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò
+                            per preparare un testo campione. È sopravvissuto non solo a più di cinque secoli, ma
+                            anche al passaggio alla videoimpaginazione, pervenendoci sostanzialmente inalterato.
+                            </p>
+                            <h4 class="text-center">Acquista qualsiasi biglietto usando servizio Tim Trasporti e avrai
+                                Tim
+                                Tag in regalo</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <span class="promo-trial"><p class="text-center">!</p></span>
+                    <div class="thumbnail sl-offerta">
+                        <p>
+                            <img class="media-object" src="http://placehold.it/200x150" align="left" alt="First">
+                        <h4 class="media-heading">Scopri nuove storie</h4>
+                        Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della
+                        stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo
+                        secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò
+                        per preparare un testo campione. È sopravvissuto non solo a più di cinque secoli, ma
+                        anche al passaggio alla videoimpaginazione, pervenendoci sostanzialmente inalterato.
+                        </p>
+                        <h4 class="text-center">Per tutti gli utenti TIM un mese di prova gratuita</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+<?php
+include 'include/footer.php';
+?>
