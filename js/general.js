ab5f5a84f7190fd5f13eddf2cbd1af27945ca63a
Davide Piantella <davide.piantella@gmail.com>
2016-06-30 18:40:59 +0200

    Phonegap stuff in script.js

--- js/general.js
+++ js/general.js
@@ -1,3 +1,10 @@
+// Phonegap stuff
+$(document).bind("mobileinit", function () { 
+	$.support.cors = true; 
+	$.mobile.allowCrossDomainPages = true; 
+});
+
+
 function getURLParameter(name) {
   return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
 }
