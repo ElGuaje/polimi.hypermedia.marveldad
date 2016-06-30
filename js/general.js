14b141ce1882303f4ea807aa76c0d91a11dff9b8
Davide Piantella <davide.piantella@gmail.com>
2016-06-30 19:01:19 +0200

    Local domain config

--- js/general.js
+++ js/general.js
@@ -7,7 +7,7 @@ $.ajaxSetup({
 	crossDomain: true
 });
 
-var mySite = 'http://127.0.0.1/hypermedia/';
+var mySite = '';
 
 function getURLParameter(name) {
   return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
