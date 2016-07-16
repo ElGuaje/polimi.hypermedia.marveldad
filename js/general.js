a7dff8df635a00d62e749e6334345a09e051f83d
Marco <blackhammer94@gmail.com>
2016-07-16 14:31:10 +0200

    fixes issue with js script for multi-image carousel, moved to a script tag in group pageù

--- js/general.js
+++ js/general.js
@@ -33,55 +33,5 @@ function ajaxErrorRep() {
     if (feed != undefined)
         if (feed.toLowerCase() == 'yes' || feed.toLowerCase() == 'si' || feed.toLowerCase() == 'y' || feed.toLowerCase() == 's')
             alert('Thanks, they really appreciated that!\nBut unfortunately the error is still there :(');
-};
-
-(function($) {
-    $(function() {
-        var jcarousel = $('.jcarousel');
-
-        jcarousel
-            .on('jcarousel:reload jcarousel:create', function () {
-                var carousel = $(this),
-                    width = carousel.innerWidth();
-
-                if (width >= 600) {
-                    width = width / 2;
-                } else if (width >= 350) {
-                    width = width / 2;
-                }
-
-                carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
-            })
-            .jcarousel({
-                wrap: 'circular'
-            });
-
-        $('.jcarousel-control-prev')
-            .jcarouselControl({
-                target: '-=1'
-            });
-
-        $('.jcarousel-control-next')
-            .jcarouselControl({
-                target: '+=1'
-            });
-
-        $('.jcarousel-pagination')
-            .on('jcarouselpagination:active', 'a', function() {
-                $(this).addClass('active');
-            })
-            .on('jcarouselpagination:inactive', 'a', function() {
-                $(this).removeClass('active');
-            })
-            .on('click', function(e) {
-                e.preventDefault();
-            })
-            .jcarouselPagination({
-                perPage: 1,
-                item: function(page) {
-                    return '<a href="#' + page + '">' + page + '</a>';
-                }
-            });
-    });
-})(jQuery);
+};  
 
