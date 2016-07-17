31830823edad95877b999e4f251e9ff3f14c34e9
Pavel Panyushov <panyushov@gmail.com>
2016-07-17 19:01:44 +0200

    Carousel fix

--- js/general.js
+++ js/general.js
@@ -7,15 +7,7 @@ $.ajaxSetup({
     crossDomain: true
 });
 
-
-
-
-
-
-
-
-
-var mySite = '';
+var mySite = 'http://marveldad.altervista.org/';
 
 function getURLParameter(name) {
     return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
@@ -27,10 +19,10 @@ var imgPathSL = mySite + 'media/sl_images/';
 var imgPathAss = mySite + 'media/assistenza/';
 
 $(document).ready(function () {
-    $('#divNavbar').load('include/navigation.html',function(){
-		if(typeof setActivePageCaller == 'function') 
-			setActivePageCaller();
-		});
+    $('#divNavbar').load('include/navigation.html', function () {
+        if (typeof setActivePageCaller == 'function')
+            setActivePageCaller();
+    });
     $('#divFooter').load('include/footer.html');
 });
 
@@ -44,9 +36,66 @@ function ajaxErrorRep() {
     if (feed != undefined)
         if (feed.toLowerCase() == 'yes' || feed.toLowerCase() == 'si' || feed.toLowerCase() == 'y' || feed.toLowerCase() == 's')
             alert('Thanks, they really appreciated that!\nBut unfortunately the error is still there :(');
-};  
+};
 
-function setActivePage(page){
-	$(page).find('a').addClass('currentPageNav');
+function setActivePage(page) {
+    $(page).find('a').addClass('currentPageNav');
 }
 
+$(document).ready(function () {
+    $('a[data-carousel="true"]').on('shown.bs.tab', function (e) {
+        var target = $(e.target).attr("href");
+
+        (function ($) {
+            $(function () {
+                var jcarousel = $('.jcarousel');
+
+                jcarousel.on('jcarousel:reload jcarousel:create', function () {
+                        var carousel = $(this),
+                            width = carousel.innerWidth();
+
+
+                        if (width >= 600) {
+                            width = width / 2;
+                        } else if (width >= 350) {
+                            width = width / 2;
+                        }
+
+                        carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
+                    })
+                    .jcarousel({
+                        wrap: 'circular'
+                    });
+
+                $('.jcarousel-control-prev')
+                    .jcarouselControl({
+                        target: '-=1'
+                    });
+
+                $('.jcarousel-control-next')
+                    .jcarouselControl({
+                        target: '+=1'
+                    });
+
+                $('.jcarousel-pagination')
+                    .on('jcarouselpagination:active', 'a', function () {
+                        $(this).addClass('active');
+                    })
+                    .on('jcarouselpagination:inactive', 'a', function () {
+                        $(this).removeClass('active');
+                    })
+                    .on('click', function (e) {
+                        e.preventDefault();
+                    })
+                    .jcarouselPagination({
+                        perPage: 1,
+                        item: function (page) {
+                            return '<a href="#' + page + '">' + page + '</a>';
+                        }
+                    });
+
+            });
+        })(jQuery);
+    });
+
+});
